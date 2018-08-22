c346d5640399aa62a6e9fe66123a7459c03ef870
Moto42 <moto42@gmail.com>
2018-08-22 07:27:56 -0500

    Moving Forward

--- src/components/AlertsDisplay/AlertBox/AlertBox.js
+++ src/components/AlertsDisplay/AlertBox/AlertBox.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import './AlertBox.css';
 
 class AlertBox extends Component  {
+
 	render()  {
-		console.log(this.props.alertData);
-	return (<div className='alertBox'>
+		let now = new Date().getTime();
+		const expiry = Date.UTC(2018,7,23,0,19,4,0);
+		let timeRemaining = expiry-now;
+		console.log('Now: ',now,);
+		console.log('expiry: ',expiry);
+		console.log('remaining: ',timeRemaining);
+
+
+	return (<div className='alertBox panelBorders'>
 		<img className='rewardImage' 
 			alt={this.props.alertData.mission.reward.itemString}
 			src={this.props.alertData.mission.reward.thumbnail} 
@@ -12,9 +20,11 @@ class AlertBox extends Component  {
 
 		<h1 className='missionData textWhite'>DEFAULT</h1>
 		
-		<img  className='factionIcon' />
+		<img className='factionIcon'
+			alt={this.props.alertData.mission.faction}
+		/>
 		
-		<h2 className='countDown'>Time is running out</h2>
+		<h2 className='countDown textGrey'>Time is running out</h2>
 
 	</div>)
 
--- src/components/AlertsDisplay/AlertsDisplay.js
+++ src/components/AlertsDisplay/AlertsDisplay.js
@@ -5,30 +5,30 @@ import AlertBox from './AlertBox';
 const alertData = {
 	id: "5b7c9fc551bb35b19c83e3cd",
 	activation: "2018-08-21T23:31:45.615Z",
-	expiry: "2018-08-22T00:19:44.040Z",
+	expiry: "2018-08-23T00:19:44.040Z",
 	mission: {
-	node: "Brugia (Eris)",
-	type: "Defense",
-	faction: "Infested",
-	reward: {
-		items: [ ],
-		countedItems: [
-			{
-				count: 1500,
-				type: "Circuits"
-			}
-		],
-		credits: 12500,
-		asString: "1500 Circuits + 12500cr",
-		itemString: "1500 Circuits",
-		thumbnail: "https://i.imgur.com/OxJvWIx.png",
-		color: 13170943
-	},
-	minEnemyLevel: 35,
-	maxEnemyLevel: 39,
-	maxWaveNum: 10,
-	nightmare: false,
-	archwingRequired: false
+		node: "Brugia (Eris)",
+		type: "Defense",
+		faction: "Infested",
+		reward: {
+			items: [ ],
+			countedItems: [
+				{
+					count: 1500,
+					type: "Circuits"
+				}
+			],
+			credits: 12500,
+			asString: "1500 Circuits + 12500cr",
+			itemString: "1500 Circuits",
+			thumbnail: "https://i.imgur.com/OxJvWIx.png",
+			color: 13170943
+		},
+		minEnemyLevel: 35,
+		maxEnemyLevel: 39,
+		maxWaveNum: 10,
+		nightmare: false,
+		archwingRequired: false
 	},
 	expired: false,
 	eta: "8m 42s",
